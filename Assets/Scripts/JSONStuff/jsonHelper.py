3cf6bdd06c6d0f1c173e1d23a92395701346af1c
1discordeclipse1 <110547466+1discordeclipse1@users.noreply.github.com>
2022-12-07 11:00:00 +0000

    update jsonHelper.py

--- Assets/Scripts/JSONStuff/jsonHelper.py
+++ Assets/Scripts/JSONStuff/jsonHelper.py
@@ -4,6 +4,7 @@ window=Tk()
 # add widgets here
 properties = ['military','economy','religion','food','social']
 advisors = ['militaryA','economyA','religionA','foodA','socialA','catA']
+sounds = ['bearDieWithFire','happyBearNoises','ratEatingNoises','ratPainNoise']
 
 def callback(self, P):
     if str.isdigit(P) and int(P) >= 0 and int(P) <= 10:
@@ -15,137 +16,98 @@ def retrieve_input(textBox):
     inputValue=textBox.get("1.0","end-1c")
     return inputValue
 
-tfDesc=Text(window,height = 5, width=40)
-tfDesc.grid(row=0,column=1)
 
-variable = StringVar(window)
-omAdv=OptionMenu(window,variable,*advisors)
-omAdv.grid(row=1,column=1)
+widgets = []
+h = 1
+m = 3
+w = 40
 
-v1 = StringVar()
-l1 = Label(window, textvariable=v1)
-v1.set("Responds yes")
-l1.grid(row=2,column=1)
+description = Text(height = h*m, width=w)
+op1=StringVar()
+advisor = OptionMenu(window,op1,*advisors)
+s=StringVar()
+sfx = OptionMenu(window,s,*sounds)
+decisionDesc = Text(height = h*m, width=w)
+widgets.append(description)
+widgets.append(advisor)
+widgets.append(sfx)
+widgets.append(decisionDesc)
 
+decision1desc = Text(height = h*m, width=w)
+decision1but = Text(height = h, width=w)
+s11=StringVar()
+stat11 = OptionMenu(window,s11,*properties)
+stat11amount = Text(height = h, width=w)
+s12 = StringVar()
+stat12 = OptionMenu(window,s12,*properties)
+stat12amount = Text(height = h, width=w)
+s1 = StringVar()
+sfx1 = OptionMenu(window,s1,*sounds)
+widgets.append(decision1desc)
+widgets.append(decision1but)
+widgets.append(stat11)
+widgets.append(stat11amount)
+widgets.append(stat12)
+widgets.append(stat12amount)
+widgets.append(sfx1)
 
-variable11 = StringVar(window)
-oms11=OptionMenu(window,variable11,*properties)
-oms11.grid(row=3,column=1)
+decision2desc = Text(height = h*m, width=w)
+decision2but = Text(height = h, width=w)
+s21 = StringVar()
+stat21 = OptionMenu(window,s21,*properties)
+stat21amount = Text(height = h, width=w)
+s22 = StringVar()
+stat22 = OptionMenu(window,s22,*properties)
+stat22amount = Text(height = h, width=w)
+s2 = StringVar()
+sfx2 = OptionMenu(window,s2,*sounds)
+widgets.append(decision2desc)
+widgets.append(decision2but)
+widgets.append(stat21)
+widgets.append(stat21amount)
+widgets.append(stat22)
+widgets.append(stat22amount)
+widgets.append(sfx2)
 
-variable12 = StringVar(window)
-oms12=OptionMenu(window,variable12,*properties)
-oms12.grid(row=4,column=1)
-
-tfDesc1=Text(window,height = 5, width=40)
-tfDesc1.grid(row=5,column=1)
-
-teInp11 = Entry(window)
-teInp11.grid(row = 3, column=2)
-
-
-
-teInp12 = Entry(window)
-teInp12.grid(row = 4, column=2)
-
-v1 = StringVar()
-l1 = Label(window, textvariable=v1)
-v1.set("Responds no")
-l1.grid(row=6,column=1)
-
-variable21 = StringVar(window)
-om21=OptionMenu(window,variable21,*properties)
-om21.grid(row=7,column=1)
-
-variable22 = StringVar(window)
-om22=OptionMenu(window,variable22,*properties)
-om22.grid(row=8,column=1)
-
-teInp21 = Entry(window)
-teInp21.grid(row = 7, column=2)
-
-teInp22 = Entry(window)
-teInp22.grid(row = 8, column=2)
-
-tfDesc2=Text(window,height = 5, width=40)
-tfDesc2.grid(row=9,column=1)
-
-v1 = StringVar()
-l1 = Label(window, textvariable=v1)
-v1.set("description: ")
-l1.grid(row=0,column=0)
-
-v2 = StringVar()
-l2 = Label(window, textvariable=v2)
-v2.set("advisor: ")
-l2.grid(row=1,column=0)
-
-v3 = StringVar()
-l3 = Label(window, textvariable=v3)
-v3.set("stat1: ")
-l3.grid(row=3,column=0)
-
-v4 = StringVar()
-l4 = Label(window, textvariable=v4)
-v4.set("stat2: ")
-l4.grid(row=4,column=0)
-
-v5 = StringVar()
-l5 = Label(window, textvariable=v5)
-v5.set("description: ")
-l5.grid(row=5,column=0)
-
-
-v6 = StringVar()
-l6 = Label(window, textvariable=v6)
-v6.set("stat1: ")
-l6.grid(row=7,column=0)
+def getJson():
+    j = {
+            "description": retrieve_input(description),
+            "advisor": advisor.get(),
+            "SFX": sfx.get(),
+            "decision1Desc": retrieve_input(decision1but),
+            "decision1": {
+                "description": retrieve_input(decision1desc),
+                "stat1": stat11.get(),
+                "stat1Amount": retrieve_input(stat11amount),
+                "stat2": stat12.get(),
+                "stat2Amount": retrieve_input(stat12amount),
+                "SFX": sfx1.get()
+            },
+            "decision2Desc": retrieve_input(decision2but),
+            "decision2": {
+                "description": retrieve_input(decision2desc),
+                "stat1": stat21.get(),
+                "stat1Amount": retrieve_input(stat21amount),
+                "stat2": stat22.get(),
+                "stat2Amount": retrieve_input(stat22amount),
+                "SFX": sfx2.get()
+            }
+    }
+    return j
 
 
-v7 = StringVar()
-l7 = Label(window, textvariable=v7)
-v7.set("stat2: ")
-l7.grid(row=8,column=0)
+bgen = Button(command=getJson,text="Generate")
+widgets.append(bgen)
 
-
-v8 = StringVar()
-l8 = Label(window, textvariable=v8)
-v8.set("description: ")
-l8.grid(row=9,column=0)
-
-def printJson():
-    j = json.dumps({
-        'description':retrieve_input(tfDesc), 
-        'advisor':variable.get(), 
-        'consequence':[{
-            'desicion':True,
-            'stat1':variable11.get(),
-            'mod1':teInp11.get(),
-            'stat2':variable12.get(),
-            'mod2':teInp12.get(),
-            'description':retrieve_input(tfDesc1)},
-            {'desicion':False,
-            'stat1':variable21.get(),
-            'mod1':teInp21.get(),
-            'stat2':variable22.get(),
-            'mod2':teInp22.get(),
-            'description':retrieve_input(tfDesc2)}]})
-    print(j)
-bgen = Button(window,command=printJson)
-bgen.grid(row = 10, column=1)
-
-
-
-
-
-#{descp: String, adv: Advisor, consq: {{desci:True,stat1: +int, stat2:-int}{desci:False,stat1: -int, stat2:+int}}} 
-
-
-
-#drop down menu for advisor
-
+i = 0
+for w in widgets:
+    w.grid(row=i,column=0)
+    i = i + 1
 
 window.title('Hello Python')
-window.geometry("300x200+10+20")
+width= window.winfo_screenwidth()               
+height= window.winfo_screenheight()               
+window.geometry("%dx%d" % (width, height))
 window.mainloop()
 
 
