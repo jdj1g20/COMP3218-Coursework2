5f850e57f3dbdd7440279534b672cfea3728ab8b
jdj1g20 <72460906+jdj1g20@users.noreply.github.com>
2022-12-18 16:52:18 +0000

    Started adding stats
    started adding stats

---
+++ Assets/Scripts/KingdomStatsScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingdomStatsScript : MonoBehaviour
+{
+    public int military = 5;
+    public int economy = 5;
+    public int social = 5;
+    public int civil = 5;
+    public int food = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
